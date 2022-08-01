8550afa0f46c8909d2f5a0ebd07f1d700d307be6
Hwangyunji <105402299+Hwangyunji@users.noreply.github.com>
2022-08-01 16:18:38 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+
+    // 모바일 토글 메뉴
+    //#toggle click 하면 nav가 toggle됨(toggleClass)
+    $('#toggle').click(function(){
+        $('header nav').toggleClass('on');
+    })
+
+    // slide down 효과로 수정
+})
